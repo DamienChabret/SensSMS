a438b855e3f7b7b6739d0e17dece119a2dfa7d6d
Damien CHABRET <104843206+DamienChabret@users.noreply.github.com>
2022-09-09 20:11:46 +0200

    Add files via upload

---
+++ SendMessageViaSMS/SendMessageViaSMS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+
+namespace SendMessageViaSMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "+33661782612";
+            string message = "SSAAAAAAAAAAALUTG";
+            SendMessage.SendSMS(number, message);
+        }
+    }
+}
+
---
+++ SendMessageViaSMS/SendMessageViaSMS/SendMessage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace SendMessageViaSMS
+{
+    /// <summary>
+    /// Classe qui permet d'envoyer un message à un numéro de téléphone
+    /// </summary>
+    public static class SendMessage
+    {
+        /// <summary>
+        /// Méthode qui permet d'envoyer un sms 
+        /// </summary>
+        /// <param name="number"> Numéro de téléphone de la personne qui va recevoir le sms </param>
+        public static void SendSMS(string number, string sms)
+        {
+            
+            var accountSid = "AC9f60af347996b8e8c40d935c1000cb46"; // SID du compte Twilio
+            var authToken = "034f4ed26da6c104a7d28683a35ab1ce"; // Token de son compte Twilio
+            TwilioClient.Init(accountSid, authToken);
+
+            // Message qui va être envoyé
+            var message = MessageResource.Create(
+                body: sms,
+                from: new Twilio.Types.PhoneNumber("+14013869852"),
+                to: new Twilio.Types.PhoneNumber(number));
+
+            Console.WriteLine(message.Body);
+
+        }
+
+
+    }
+}
